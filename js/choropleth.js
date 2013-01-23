81c9412b1bb1216077d4857834cd66cb9f9c8929
Juan Mendívil <juan@mendivil.mx>
2013-01-23 02:29:06 -0600

    add: choropleth object literal notation

--- js/choropleth.js
+++ js/choropleth.js
@@ -1,25 +1,37 @@
-function choropleth(select, data){
-				//arreglo con valores del data
-	var mval	= [],
-				//colores de menos a más
-		colors	= ["#EDF8E9","#BAE4B3","#74C476","#238B45"];
+var choropleth = {
+	do : function (select, data, colors) {
+		//checa que el documento este listo
 
-	for(l in data){
-		mval.push(data[l])}
+		var readyStateCheckInterval = setInterval(function() {
+			if (document.readyState === "complete") {
+
+				//arreglo con valores del data
+				var mval	= [];
+
+				for(l in data){
+					mval.push(data[l])}
 
 				//número de niveles según los colores
-	var lvls	= colors.length-1,
-				//valor máximo en data
-		lMax	= Math.max.apply(Math,mval), 
-				//svg
-		map		= document.getElementById(select).contentDocument,
-				//cuantiza
-		q		= function(select){return~~(select*lvls	/lMax)};
+				var lvls	= colors.length-1,
+					//valor máximo en data
+					lMax	= Math.max.apply(Math,mval), 
+					//svg
+					map		= document.getElementById(select).contentDocument,
+					//cuantiza
+					q		= function(select){return~~(select*lvls	/lMax)};
 
-	for(r in data){
-		var region	= r,
-			val		= data[r],
-			rcol	= map.getElementById(region);
-		rcol.style.fill = colors[q(val)]
+				for(r in data){
+					var region	= r,
+						val		= data[r],
+								rcol	= map.getElementById(region);
+					rcol.style.fill = colors[q(val)]
+				}
+
+				//elimino el intervalo para checar documento listo
+				clearInterval(readyStateCheckInterval);
+
+			}
+		}, 10);
+
 	}
 };
